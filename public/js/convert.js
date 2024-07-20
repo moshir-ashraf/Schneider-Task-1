98f82c1218c27333a7d8b7e2d0165f453c4b76e4
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2024-07-20 18:08:43 +0300

    Added Formatting

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,32 +1,30 @@
-let inputs = document.querySelectorAll('input')
-let labels = document.querySelectorAll('label')
-let switcher = document.getElementById('switch')
-let convert = document.getElementById('convert')
+let inputs = document.querySelectorAll("input");
+let labels = document.querySelectorAll("label");
+let switcher = document.getElementById("switch");
+let convert = document.getElementById("convert");
 
-switcher.addEventListener('click',function() {
-    if(labels[0].innerHTML == 'Celsius'){
-        labels[0].innerHTML = 'Fahrenheit'
-        inputs[0].placeholder = 'Enter Temperature in Fahrenheit'
-        labels[1].innerHTML = 'Celsius'
-        inputs[1].placeholder = 'Temperature in Celsius'
+switcher.addEventListener("click", function () {
+    if (labels[0].innerHTML == "Celsius") {
+        labels[0].innerHTML = "Fahrenheit";
+        inputs[0].placeholder = "Enter Temperature in Fahrenheit";
+        labels[1].innerHTML = "Celsius";
+        inputs[1].placeholder = "Temperature in Celsius";
+    } else if (labels[0].innerHTML == "Fahrenheit") {
+        labels[0].innerHTML = "Celsius";
+        inputs[0].placeholder = "Enter Temperature in Celsius";
+        labels[1].innerHTML = "Fahrenheit";
+        inputs[1].placeholder = "Temperature in Fahrenheit";
     }
-    else if(labels[0].innerHTML == 'Fahrenheit'){
-        labels[0].innerHTML = 'Celsius'
-        inputs[0].placeholder = 'Enter Temperature in Celsius'
-        labels[1].innerHTML = 'Fahrenheit'
-        inputs[1].placeholder = 'Temperature in Fahrenheit'
-    }
-})
+});
 
-convert.addEventListener('click',function() {
-    if(labels[0].innerHTML == 'Celsius'){
-        let celsius = inputs[0].value
-        let fahrenheit = (celsius * 9/5) + 32
-        inputs[1].value = fahrenheit.toFixed(2)
+convert.addEventListener("click", function () {
+    if (labels[0].innerHTML == "Celsius") {
+        let celsius = inputs[0].value;
+        let fahrenheit = (celsius * 9) / 5 + 32;
+        inputs[1].value = fahrenheit.toFixed(2);
+    } else if (labels[0].innerHTML == "Fahrenheit") {
+        let fahrenheit = inputs[0].value;
+        let celsius = ((fahrenheit - 32) * 5) / 9;
+        inputs[1].value = celsius.toFixed(2);
     }
-    else if(labels[0].innerHTML == 'Fahrenheit'){
-        let fahrenheit = inputs[0].value
-        let celsius = (fahrenheit - 32) * 5/9
-        inputs[1].value = celsius.toFixed(2)
-    }
-})
+});
